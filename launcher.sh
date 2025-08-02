6e044e4fb7a31dd45163a1ce1ab662b2667f9132
KARAN <143735873+punkkkkkkk@users.noreply.github.com>
2025-08-02 23:03:41 +0530

    Create launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Karan's Launcher Script - Executes the main prank automatically
+# This simple script just runs the main prank command
+
+/opt/homebrew/bin/prank.sh /opt/homebrew/share/dingdong/payload.png
